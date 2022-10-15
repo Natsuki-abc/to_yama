0b1db8b67220183036c639037e4ddc9b2e1c5da5
今井菜月 <natsukiimai@imainatsukinoMacBook-Air.local>
2022-10-15 11:15:19 +0900

    prepare heroku_deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
 
-  root :to => 'users#index'
+  resources :users
+  root 'users#index'
 
   devise_for :users
-
-  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
