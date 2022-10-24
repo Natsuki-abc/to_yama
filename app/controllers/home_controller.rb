0711df67c4eaf1eadb628aec00554e99fcb65373
今井菜月 <natsukiimai@imainatsukinoMacBook-Air.local>
2022-10-24 20:45:04 +0900

    edit devise view

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,14 +1,4 @@
 class HomeController < ApplicationController
   def index
   end
-
-  def guest_sign_in
-    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
-      user.name = "ゲストユーザー"
-      user.password = SecureRandom.urlsafe_base64
-      # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
-    end
-    sign_in user
-    redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,15 @@ Rails.application.routes.draw do
 
   resources :home, only: [:index]
   root 'home#index'
-  post '/home/guest_sign_in', to: 'home#guest_sign_in'
 
-  devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    passwords: 'users/passwords'
+  }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
