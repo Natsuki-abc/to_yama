89bb516058c7c01025c7e6676f9e47db8a239f27
今井菜月 <natsukiimai@imainatsukinoMacBook-Air.local>
2022-10-30 21:02:33 +0900

    fixed home#index,registrations view

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "名前、メールアドレス、パスワードが入力されている場合、登録できること" do
+  it "名前、メールアドレス、パスワードが入力されている場合、有効であること" do
     user = User.new(
     name: "太郎",
     email: "test@example.com",
     password: "password",
     )
-    expect(user).to be_valid
+    expect(user).to be true
+  end
+
+  it "名前がない場合、無効であること" do
+
+  end
+
+  it "メールアドレスがない場合、無効であること" do
+  end
+
+  it "パスワードがない場合、無効であること" do
   end
 end
