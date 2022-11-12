5bbe611ecd311bf7e11ca960b58938dfa604c51d
今井菜月 <natsukiimai@imainatsukinoMacBook-Air.local>
2022-11-12 22:10:46 +0900

    add Users model to admin column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_11_092308) do
+ActiveRecord::Schema.define(version: 2022_11_12_130722) do
 
   create_table "mountains", force: :cascade do |t|
     t.string "mountain_name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2022_11_11_092308) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name", null: false
     t.string "icon"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
