87f75746140cfb9230e6a7047b83ad6735a1a1eb
今井菜月 <natsukiimai@imainatsukinoMacBook-Air.local>
2022-11-22 20:55:46 +0900

    add mountain/model_spec

--- spec/factories/mountain.rb
+++ spec/factories/mountain.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :mountain do
-    mountain_name { Faker::Lorem.characters(number: 5) }
-    address {  }
-    time {  }
-    elevation {  }
-    difficulty {  }
-    station {  }
-    season {  }
-    parking {  }
-    distance {  }
+    mountain_name { Faker::Lorem.words(number: 5) }
+    address { Faker::Address.state }
+    time { Faker::Lorem.characters(number: 6) }
+    elevation { Faker::Number.number(digits: 5) }
+    difficulty { Faker::Number.number(digits: 1) }
+    station { Faker::String.random(length: 15..30) }
+    season { Faker::String.random(length: 3..10) }
+    parking { Faker::String.random(length: 10..20) }
+    distance { Faker::Number.number(digits: 2) }
   end
 end
