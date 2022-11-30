b003db2b61fbe18754e039a5ea0e79acd1018410
今井菜月 <natsukiimai@imainatsukinoMacBook-Air.local>
2022-11-30 21:38:57 +0900

    edit mountains_controller to fix search(free word)

--- app/controllers/mountains_controller.rb
+++ app/controllers/mountains_controller.rb
@@ -15,8 +15,10 @@ class MountainsController < ApplicationController
     @results = @q.result
     if @q.address_cont.present?
       @search_word = @q.address_cont
-    else
+    elsif @q.area_cont.present? 
       @search_word = @q.area_cont
+    else
+      @search_word = @q.mountain_name_or_address_or_elevation_or_station_or_season_or_parking_or_distance_or_area_or_furigana_cont
     end
   end
 
