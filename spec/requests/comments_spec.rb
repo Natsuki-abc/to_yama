d4807cce7ddc81ae3e5cf7bb5ebffc436ba65e56
今井菜月 <natsukiimai@imainatsukinoMacBook-Air.local>
2022-12-03 20:59:43 +0900

    add spec/request/comment,home

--- spec/requests/comments_spec.rb
+++ spec/requests/comments_spec.rb
@@ -1,11 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe "Comments", type: :request do
-  describe "GET /index" do
-    it "returns http success" do
-      get "/comments/index"
-      expect(response).to have_http_status(:success)
+RSpec.describe 'Comments', type: :request do
+  describe 'GET /index' do
+    before do
+      @user = FactoryBot.create(:user)
+      sign_in @user
+      @mountain = FactoryBot.create(:mountain)
+      @comment = FactoryBot.create(:comment)
+    end
+
+    it 'statusが200であること' do
+      get comments_path
+      expect(response).to have_http_status(200)
     end
   end
-
 end
