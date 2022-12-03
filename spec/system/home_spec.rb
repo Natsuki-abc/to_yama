ec490dac21b6c07620226fe6687d58acbaed8d14
今井菜月 <natsukiimai@imainatsukinoMacBook-Air.local>
2022-12-03 21:44:42 +0900

    add spec/system/home

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -1,19 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "Home#index", type: :system do
-  describe "Header" do
-    before do
-      visit root_path
-    end
+RSpec.describe 'Home#index', type: :system do
+  before do
+    visit root_path
+  end
 
-    it "TOPボタンを押すと、TOPページにアクセスできること" do
-      click_link "TOP"
+  describe 'Header' do
+    it 'TOPボタンを押すと、TOPページにアクセスできること' do
+      click_link 'TOP'
       expect(current_path).to eq root_path
     end
 
-    it "「山を探す」ボタンを押すと、mountain#indexにアクセスできること" do
-      click_link "山を探す"
+    it '「山を探す」ボタンを押すと、mountain#indexにアクセスできること' do
+      click_link '山を探す'
       expect(current_path).to eq mountains_path
+    end
+  end
+
+  describe 'Footer' do
+    it '「お問合せ（山の追加希望もこちら）」ボタンを押すと、home#newにアクセスできること' do
+      click_link 'お問合せ（山の追加希望もこちら）'
+      expect(current_path).to eq new_home_path
     end
   end
 end
