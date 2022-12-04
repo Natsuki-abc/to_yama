8062e9375dde6001e4cc791b104ad3aa55198e4d
今井菜月 <natsukiimai@imainatsukinoMacBook-Air.local>
2022-12-04 16:51:54 +0900

    add model spec to comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Commentの新規登録が有効な場合' do
+    it '必須情報が全て入力されていると、有効であること' do
+      expect(FactoryBot.create(:comment)).to be_valid
+    end
+  end
+
+  context 'Commentの新規登録が無効な場合' do
+    it 'タイトルが入力されていない場合、無効であること' do
+      expect(FactoryBot.build(:comment, title: '')).not_to be_valid
+    end
+
+    it 'コメント内容が入力されていない場合、無効であること' do
+      expect(FactoryBot.build(:comment, content: '')).not_to be_valid
+    end
+  end
 end
