3900d544aa25445adc69d5786d9a89b8a6a0b774
今井菜月 <natsukiimai@imainatsukinoMacBook-Air.local>
2022-12-04 16:01:19 +0900

    add system spec to home

--- spec/system/home_spec.rb
+++ spec/system/home_spec.rb
@@ -17,6 +17,27 @@ RSpec.describe 'Home#index', type: :system do
     end
   end
 
+  describe 'Main' do
+    it 'home#newに移管すること' do
+      click_link 'View details »', match: :first
+      expect(current_path).to eq new_home_path
+    end
+
+    it 'mountain#indexに移管すること' do
+      within '.home_2' do
+        click_link 'View details »'
+        expect(current_path).to eq mountains_path
+      end
+    end
+
+    it 'home#newページに移管すること' do
+      within '.home_3' do
+        click_link 'View details »'
+        expect(current_path).to eq new_home_path
+      end
+    end
+  end
+
   describe 'Footer' do
     it '「お問合せ（山の追加希望もこちら）」ボタンを押すと、home#newにアクセスできること' do
       click_link 'お問合せ（山の追加希望もこちら）'
