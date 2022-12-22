c6beef90d43254d1d2e75bd87469de4250f6dce3
今井菜月 <natsukiimai@imainatsukinoMacBook-Air.local>
2022-12-22 21:57:47 +0900

    fixed error:unicorn undefined local variable or method root_path

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,7 +2,7 @@ app_path = File.expand_path('../../', __FILE__)
 
 worker_processes 2
 
-working_directory /var/www/to_yama
+working_directory=/var/www/to_yama
 
 pid "#{root_path}/tmp/pids/unicorn.pid"
 
