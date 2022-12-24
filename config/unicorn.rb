9ba23ad470e97d0f75e89d4cc5384d78a55ce8ea
今井菜月 <natsukiimai@imainatsukinoMacBook-Air.local>
2022-12-24 09:58:10 +0900

    to fix error:Already running on PID:12965

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,7 +2,7 @@ app_path = File.expand_path('../../', __FILE__)
 
 worker_processes 2
 
-working_directory "/var/www/to_yama"
+working_directory "#{app_path}/current"
 
 pid "#{app_path}/tmp/pids/unicorn.pid"
 
