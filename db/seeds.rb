4650a606fcc9a212e7093e4169cf79fdbf389e05
今井菜月 <natsukiimai@imainatsukinoMacBook-Air.local>
2023-01-14 22:53:45 +0900

    deleted seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-User.create(email: 'guest@example.com', password: 'test1234', password_confirmation: 'test1234', name: 'ゲストユーザー',)
